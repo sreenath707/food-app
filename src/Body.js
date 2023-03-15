6be5d1b466bc68536c18f51de49139f70f976f16
sreenath707 <sreenathsharma707@gmail.com>
2023-03-15 17:11:45 +0530

    grid implemented

--- src/Body.js
+++ src/Body.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
 import FoodCard from "./components/FoodCard";
 import foodList from "./utils/Food-data";
+import Grid from '@mui/material/Grid'
 
 function Body() {
   return (
-    <div className="flex flex-wrap p-[20px]">
+    <Grid spacing={4} container className="p-[20px]" >
       {foodList.map((food) => (
         <FoodCard food={food} />
       ))}
-    </div>
+    </Grid>
   );
 }
 
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,24 +1,33 @@
+import Grid from '@mui/material/Grid'
+import StarIcon from '@mui/icons-material/Star'
+
 function FoodCard({ food }) {
   return (
-    <div className="rounded-[5px] overflow-hidden w-[300px] bg-gray-200 m-[20px]">
-      <img className="w-full h-150px" src={food.image} />
-      <div className="px-[5px] py-[10px]">
-        <div className="flex justify-between">
-          <div>{food.title}</div>
-          <div>${food.cost}</div>
-        </div>
-        <div className="text-[12px] text-gray-700">{food.description}</div>
-        <div className="w-full flex justify-between items-center">
-          <div className="flex mt-[5px] justify-between bg-red-400 px-[10px] w-[100px]">
-            <div>-</div>
-            <div>1</div>
-            <div>+</div>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <div
+        style={{
+          boxShadow:
+            'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset',
+        }}
+        className='rounded-[5px] overflow-hidden bg-gray-100'
+      >
+        <img className='w-full h-150px' alt='food-img' src={food.image} />
+        <div className='px-[10px] py-[10px]'>
+          <div className='flex justify-between'>
+            <div>{food.title}</div>
+            <div>${food.cost}</div>
           </div>
-          <div>{food.rating}</div>
+          <div className='text-[12px] text-gray-700'>{food.description}</div>
+          <div className='w-full flex justify-end items-center'>
+            <div className='bg-green-600 p-[5px] rounded-[5px] text-[14px] text-white flex items-center'>
+              <StarIcon fontSize="small" />
+              <div>{food.rating}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    </Grid>
+  )
 }
 
-export default FoodCard;
+export default FoodCard
